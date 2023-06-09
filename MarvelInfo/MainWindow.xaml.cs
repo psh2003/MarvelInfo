7894e6d3518e3f600a46773eafec56512aea8abd
psh2003 <83701900+psh2003@users.noreply.github.com>
2023-06-10 02:41:47 +0900

    개봉예정작 조회기능 & 관련 코믹스 조회기능 구현 완료

--- MarvelInfo/MainWindow.xaml.cs
+++ MarvelInfo/MainWindow.xaml.cs
@@ -10,18 +10,43 @@ using System.Text;
 using System.Web;
 using System.Globalization;
 using System.Security.Cryptography;
+using System.Windows.Documents;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace MarvelInfo
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private const string NextMarvelFilmEndpoint = "https://www.whenisthenextmcufilm.com/api";
         private const string MarvelAPIEndpoint = "https://gateway.marvel.com:443/v1/public/series?";
+        private const string MarvelAPIEndpoint2 = "https://gateway.marvel.com:443/v1/public/characters?";
+        private const string MarvelAPIEndpoint3 = "https://gateway.marvel.com:443/v1/public/series?";
         private const string apiKey = "69b2dcec13e3059839e0e7a5b957efd9";
         private const string hash = "37c882cceb8777d6616fe502f276c39c396d23ae";
 
         private HttpClient _httpClient;
+        private ObservableCollection<items> _board;
 
+        public ObservableCollection<items> Board
+        {
+            get { return _board; }
+            set
+            {
+                _board = value;
+                OnPropertyChanged(nameof(Board));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         private string GetMd5Hash(string input)
         {
             using (MD5 md5 = MD5.Create())
@@ -40,10 +65,13 @@ namespace MarvelInfo
         }
 
 
+
         public MainWindow()
         {
             InitializeComponent();
             _httpClient = new HttpClient();
+            Board = new ObservableCollection<items>();
+            this.DataContext = this;
         }
 
         private async void LoadMoviesButton_Click(object sender, RoutedEventArgs e)
@@ -55,15 +83,36 @@ namespace MarvelInfo
 
                 // 영화 정보를 ListBox에 표시
                 NextMarvelFilm nextMarvelFilm = nextFilm;
-                /*img.Source = new BitmapImage(new Uri(nextMarvelFilm.poster_url, UriKind.Absolute));*/
-                imgTxt.Text = "제목:" + nextMarvelFilm.title + "개봉일:" + nextMarvelFilm.release_date + "타입:" + nextMarvelFilm.type;
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(nextFilm.poster_url, UriKind.Absolute);
+                bitmap.EndInit();
+
+                img.Source = bitmap;
+                imgTxt.Text = nextMarvelFilm.title;
+                overview.Text = nextMarvelFilm.overview;
+                releaseDate.Text = nextMarvelFilm.release_date.ToString();
+                untilDate.Text = nextMarvelFilm.days_until.ToString();
+                type.Text = nextMarvelFilm.type;
+
 
                 // Marvel API를 사용하여 검색 결과 가져오기
-                var characters = await SearchMarvelCharacters(nextMarvelFilm.title);
+                List<MarvelApiResponseData> characters = await SearchMarvelCharacters(nextMarvelFilm.title);
 
                 // 검색 결과를 ListBox에 표시
-                CharactersListBox.ItemsSource = characters;
-                
+                /*CharactersListBox.ItemsSource = characters;*/
+
+                foreach (MarvelApiResponseData a in characters)
+                {
+                    var fullFilePath = @a.thumbnail["path"] + "." + a.thumbnail["extension"];
+
+                    bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+                    bitmap.EndInit();
+                    Board.Add(new items() { Title = a.title, ImageData = bitmap, Description = a.description});
+                }
             }
             catch (Exception ex)
             {
@@ -82,7 +131,8 @@ namespace MarvelInfo
                 var characters = await SearchMarvelCharacters(searchTerm);
 
                 // 검색 결과를 ListBox에 표시
-                CharactersListBox.ItemsSource = characters;
+                /*CharactersListBox.ItemsSource = characters;*/
+                
             }
             catch (Exception ex)
             {
@@ -103,7 +153,7 @@ namespace MarvelInfo
             return nextFilm;
         }
 
-        private async Task<List<NextMarvelFilm>> SearchMarvelCharacters(string searchTerm)
+        private async Task<List<MarvelApiResponseData>> SearchMarvelCharacters(string searchTerm)
         {
             long ts = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
             string hash2 = GetMd5Hash(ts + hash + apiKey);
@@ -114,7 +164,10 @@ namespace MarvelInfo
 
             var json = await response.Content.ReadAsStringAsync();
 
-            var characters = JsonSerializer.Deserialize<List<NextMarvelFilm>>(json);
+            var jsonObject = JsonDocument.Parse(json).RootElement;
+            var data = jsonObject.GetProperty("data").GetProperty("results");
+
+            var characters = JsonSerializer.Deserialize<List<MarvelApiResponseData>>(data);
 
             return characters;
         }
@@ -130,10 +183,22 @@ namespace MarvelInfo
         public string type { get; set; }
     }
 
-    public class Characters
+    public class MarvelApiResponseData
     {
-        public string Name { get; set; }
-        public string esourceUri { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int startYear { get; set; }
+        public int endYear { get; set; }
+        public string rating { get; set; }
+        public Dictionary<string, string> thumbnail { get; set; }
+    }
+
+    public class items
+    {
+        public BitmapImage ImageData { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 
 }
